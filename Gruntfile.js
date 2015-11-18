9133c4ef75eb807b0400abebdd341628a2ce05be
Fabien LOISON <flo@flogisoft.com>
2015-11-18 11:01:53 +0100

    + jshint checks

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,15 +17,34 @@ module.exports = function (grunt) {
 
         mocha_phantomjs: {
             all: ["test/browser/test.html"]
+        },
+
+        jshint: {
+            all: ["lib/*.js", "bin/*"],
+            options: {
+                futurehostile: true,
+                freeze: true,
+                latedef: true,
+                noarg: true,
+                nocomma: true,
+                nonbsp: true,
+                nonew: true,
+                undef: true,
+                browser: true,
+                browserify: true,
+                node: true,
+                curly: true
+            }
         }
     });
 
     grunt.loadNpmTasks("grunt-browserify");
     grunt.loadNpmTasks("grunt-mocha-test");
     grunt.loadNpmTasks("grunt-mocha-phantomjs");
+    grunt.loadNpmTasks("grunt-contrib-jshint");
 
     grunt.registerTask("default", ["test"]);
-    grunt.registerTask("test", "Run all code check and tests", ["test-node", "test-browser"]);
+    grunt.registerTask("test", "Run all code check and tests", ["jshint", "test-node", "test-browser"]);
     grunt.registerTask("test-node", "Run test on Node.js", ["mochaTest"]);
     grunt.registerTask("test-browser", "Run tests in a web browser", ["browserify:test", "mocha_phantomjs"]);
 
